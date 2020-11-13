d7b61a3147c04ecf0c6126718f9dd66e07f4a13f
Michele Colledanchise <michele.colledanchise@iit.it>
2020-11-13 16:34:17 +0100

    add components interfaces

---
+++ interfaces/components/counter.thrift
@@ -0,0 +1,11 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2020 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+service Counter {
+    int get_count();
+    void reset_counter();
+}
